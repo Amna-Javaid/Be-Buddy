cff3a50b74959e58b5e6a93f175dc6fbe44bf94a
Amna javaid <127860391+Amna-Javaid@users.noreply.github.com>
2025-12-19 12:56:53 +0500

    Delete middleware directory

--- middleware/checkCartNotEmpty.js
+++
@@ -1,8 +0,0 @@
-// middleware/checkCartNotEmpty.js
-module.exports = function checkCartNotEmpty(req, res, next) {
-    if (!req.session || !req.session.cart || req.session.cart.length === 0) {
-        req.flash && req.flash('error', 'Your cart is empty!');
-        return res.redirect('/cart');
-    }
-    next();
-};
